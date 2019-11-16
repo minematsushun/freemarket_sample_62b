226a2e34c696b3702b1def251a7e7766b197b22a
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-11-16 18:19:35 +0900

    （JS、マークアップ）トップ_スライダーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,52 +1,52 @@
 class ItemsController < ApplicationController
-  def index
+  def index #TOP
   end
 
-  def exhibit
+  def exhibit #購入内容の確認
   end
 
-  def sell
+  def sell  #商品情報の入力
   end
 
-  def myProfile
+  def myProfile #マイプロフィール
   end
 
-  def cardRegister
+  def cardRegister  #カード登録
   end
 
-  def howToPay
+  def howToPay  #支払い方法一覧
   end
 
-  def newUserRegistration
+  def newUserRegistration  #新規ユーザー登録
   end
 
-  def userLogin
+  def userLogin  #ログイン
   end
 
-  def userEdit
+  def userEdit  #ユーザー編集
   end
 
-  def userTelConfirmation
+  def userTelConfirmation  #ユーザー電話番号確認
   end
 
-  def userTelAuthentication
+  def userTelAuthentication  #ユーザー電話番号の認証
   end
 
-  def userHowToPay
+  def userHowToPay  #支払い方法登録
   end
 
-  def userAddress
+  def userAddress  #ユーザー住所
   end
 
-  def userCompRegistration
+  def userCompRegistration  #ユーザー登録完了
   end
 
-  def userLogout
+  def userLogout  #ログアウト
   end
 
-  def identification
+  def identification  #本人確認
   end
 
-  def myPage
+  def myPage  #マイページ
   end
 end
