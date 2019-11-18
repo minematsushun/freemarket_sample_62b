e8c86b7b499f9098ad031b1cbd91c2c3aefd9fc9
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-11-18 18:43:42 +0900

    モデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
   resources :items ,only: :index
+  resources :categories ,only: [:index, :show]
+  resources :brands ,only: [:index, :show]
   get "items/sell" => "items#sell"
   get "items/exhibit" => "items#exhibit"
   get "items/myProfile" => "items#myProfile"
