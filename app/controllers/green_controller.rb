a03b2083b81563b8530929567eb5474b24366e87
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-19 20:40:42 +0900

    商品出品機能実装１

---
+++ app/controllers/green_controller.rb
@@ -0,0 +1,56 @@
+class GreenController < ApplicationController
+
+  def index #TOP
+  end
+
+  def info #商品詳細
+  end
+
+  def exhibit #購入内容の確認
+  end
+
+  def sell  #商品情報の入力
+  end
+
+  def myProfile #マイプロフィール
+  end
+
+  def cardRegister  #カード登録
+  end
+
+  def howToPay  #支払い方法一覧
+  end
+
+  def newUserRegistration  #新規ユーザー登録
+  end
+
+  def userLogin  #ログイン
+  end
+
+  def userEdit  #ユーザー編集
+  end
+
+  def userTelConfirmation  #ユーザー電話番号確認
+  end
+
+  def userTelAuthentication  #ユーザー電話番号の認証
+  end
+
+  def userHowToPay  #支払い方法登録
+  end
+
+  def userAddress  #ユーザー住所
+  end
+
+  def userCompRegistration  #ユーザー登録完了
+  end
+
+  def userLogout  #ログアウト
+  end
+
+  def checkYourself  #本人確認
+  end
+
+  def myPage  #マイページ
+  end
+end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,55 +1,10 @@
 class ItemsController < ApplicationController
-  def index #TOP
+
+  def new
   end
 
-  def info #商品詳細
+  def create
+    Item.create(product_name: params[:product_name], product_text: params[:product_text], price: params[:price])
   end
 
-  def exhibit #購入内容の確認
-  end
-
-  def sell  #商品情報の入力
-  end
-
-  def myProfile #マイプロフィール
-  end
-
-  def cardRegister  #カード登録
-  end
-
-  def howToPay  #支払い方法一覧
-  end
-
-  def newUserRegistration  #新規ユーザー登録
-  end
-
-  def userLogin  #ログイン
-  end
-
-  def userEdit  #ユーザー編集
-  end
-
-  def userTelConfirmation  #ユーザー電話番号確認
-  end
-
-  def userTelAuthentication  #ユーザー電話番号の認証
-  end
-
-  def userHowToPay  #支払い方法登録
-  end
-
-  def userAddress  #ユーザー住所
-  end
-
-  def userCompRegistration  #ユーザー登録完了
-  end
-
-  def userLogout  #ログアウト
-  end
-
-  def checkYourself  #本人確認
-  end
-
-  def myPage  #マイページ
-  end
 end
