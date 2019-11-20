0ebefa7caf73f2e0b779fcd9f369c93d9a2f797a
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-20 12:43:13 +0900

    カテゴリーモデルの作成とgem導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_083329) do
+ActiveRecord::Schema.define(version: 2019_11_20_033626) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "category", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "product_name", null: false
