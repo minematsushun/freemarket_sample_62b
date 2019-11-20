f1b836fcf1d161254aef9e9423c92087da3f4f53
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-20 14:55:23 +0900

    カテゴリーテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_033626) do
+ActiveRecord::Schema.define(version: 2019_11_20_054529) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "category", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
