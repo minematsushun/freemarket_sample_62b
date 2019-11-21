a51c60911467a90f61c15fa8c7d870180ededdcc
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-11-22 00:55:44 +0900

    コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
