7ecf83ef96f46db2eca8b5366f4d3de27387eff2
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-11-22 00:10:52 +0900

    削除アクション

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -26,3 +26,27 @@ class CardController < ApplicationController
       end
     end
   end
+
+  def delete 
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "new"
+  end
+
+  def show 
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+end
