1c6a1ebddcee2b3da1ff94352e63a7246785eb9a
miyamo93 <miyamo0802@gmail.com>
2019-11-22 17:58:30 +0900

    seiyaku

--- db/migrate/20191122083823_create_items.rb
+++ db/migrate/20191122083823_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string              :product_name, null: false
-      t.integer             :price
+      t.integer             :price　, null: false
       t.timestamps
     end
   end
