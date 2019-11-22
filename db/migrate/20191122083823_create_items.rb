d47dfda49fb74a3e45cfb05f7c54918c39f46c7b
miyamo93 <miyamo0802@gmail.com>
2019-11-22 17:47:32 +0900

    制約追加

--- db/migrate/20191122083823_create_items.rb
+++ db/migrate/20191122083823_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.string              :productname,
-      t.integer
+      t.string              :product_name, null: false
+      t.integer             :price
       t.timestamps
     end
   end
