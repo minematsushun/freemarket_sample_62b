f8e595aa6d9426e5eb96df10671cb35a30b6b211
miyamo93 <miyamo0802@gmail.com>
2019-11-22 17:06:23 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_21_155257) do
-
-ActiveRecord::Schema.define(version: 2019_11_20_114209) do
-
 
   create_table "cards", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
