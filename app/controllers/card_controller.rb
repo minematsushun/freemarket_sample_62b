9324e03bffd518215e0f6b5755423255798e3a91
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-11-24 18:21:49 +0900

    登録機能 controller

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -2,12 +2,12 @@ class CardController < ApplicationController
   require "payjp"
   before_action :set_card
 
-  def new
+  def new  # カード登録
     card = Card.where(user_id: current_user.id)
     redirect_to action: "index" if card.present?
   end
 
-  def create
+  def create # payjpとCardのデータベースを作成
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
 
     if params['payjp-token'].blank?
@@ -20,31 +20,46 @@ class CardController < ApplicationController
         card: params['payjp-token'],
         metadata: {user_id: current_user.id}
       )
-  
-  
-  def delete 
-    card = Card.find_by(user_id: current_user.id)
-    if card.blank?
+    @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+    if @card.save
+      redirect_to action: "index"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      customer.delete
-      card.delete
-    end
-    redirect_to action: "new"
-  end
-  
-  def show 
-    card = Card.find_by(user_id: current_user.id)
-    if card.blank?
-      redirect_to action: "new" 
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      redirect_to action: "create"
     end
   end
 end
+  
+private
+
+  def set_card
+    @card = Card.find_by(user_id: current_user.id) if Card.where(user_id: current_user.id).present?
+  end
+end
+
+
+#   def delete # カード削除
+#     card = Card.find_by(user_id: current_user.id)
+#     if card.blank?
+#     else
+#       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+#       customer = Payjp::Customer.retrieve(card.customer_id)
+#       customer.delete
+#       card.delete
+#     end
+#     redirect_to action: "new"
+#   end
+  
+#   def show # カード情報
+#     card = Card.find_by(user_id: current_user.id)
+#     if card.blank?
+#       redirect_to action: "new" 
+#     else
+#       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+#       customer = Payjp::Customer.retrieve(card.customer_id)
+#       @default_card_information = customer.cards.retrieve(card.card_id)
+#     end
+#   end
+# end
 
 
 
