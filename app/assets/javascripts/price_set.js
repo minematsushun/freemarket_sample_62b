7e65be8f5ac80e243bd4490f35de2166bdc9c132
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-24 15:14:35 +0900

    マネー７

--- app/assets/javascripts/price_set.js
+++ app/assets/javascripts/price_set.js
@@ -8,9 +8,12 @@ $(document).on('turbolinks:load', function () {
 function moneyCalc() {
   let inputNum = $('#sell_center').val();
   let Input = parseInt(inputNum);
-  $(".sales-commission").text("---");
+  if (Input < 300 || Input)
+    $(".sales-commission").text("---");
   $(".sales-profit").text("---");
 }else {
   let fee = parseInt(Input / 10);
   $(".sales-commission").text("¥" + fee.toLocaleString());
-}
+  $(".sales-profit").text("¥" + (Input - fee).toLocaleString());
+};
+};
