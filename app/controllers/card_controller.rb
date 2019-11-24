8917bb1ffd1192ba8fdde737c50d6d6abbe48549
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-11-24 17:52:22 +0900

    コメントアウト

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -7,27 +7,7 @@ class CardController < ApplicationController
     redirect_to action: "index" if card.present?
   end
   
-  def pay 
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    if params['payjp-token'].blank?
-      redirect_to action: "new"
-      
-    else
-      customer = Payjp::Customer.create(
-        description: '登録テスト', 
-        email: current_user.email, 
-        card: params['payjp-token'],
-        metadata: {user_id: current_user.id}
-        )
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to action: "show"
-      else
-        redirect_to action: "pay"
-      end
-    end
-  end
-
+  
   def delete 
     card = Card.find_by(user_id: current_user.id)
     if card.blank?
@@ -37,7 +17,7 @@ class CardController < ApplicationController
       customer.delete
       card.delete
     end
-      redirect_to action: "new"
+    redirect_to action: "new"
   end
   
   def show 
@@ -51,3 +31,38 @@ class CardController < ApplicationController
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def pay 
+#   Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+#   if params['payjp-token'].blank?
+#     redirect_to action: "new"
+    
+#   else
+#     customer = Payjp::Customer.create(
+#       description: '登録テスト', 
+#       email: current_user.email, 
+#       card: params['payjp-token'],
+#       metadata: {user_id: current_user.id}
+#       )
+#     @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+#     if @card.save
+#       redirect_to action: "show"
+#     else
+#       redirect_to action: "pay"
+#     end
+#   end
+# end
