44d3e98fe04960f76b93c50e64fe67a4962aaaba
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-11-24 17:49:26 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -4,7 +4,7 @@ class CardController < ApplicationController
 
   def new
     card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    redirect_to action: "index" if card.present?
   end
   
   def pay 
