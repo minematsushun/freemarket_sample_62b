7e9f0f4964ccf8ee618741385ffe31857ba3c598
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-24 15:52:18 +0900

    マネー８

--- app/assets/javascripts/price_set.js
+++ app/assets/javascripts/price_set.js
@@ -1,19 +1,21 @@
 $(document).on('turbolinks:load', function () {
   moneyCalc();
-  ('#sell_center').on('keyup', function () {
-    moneyCalc();
+  $(function () {
+    (document).on('keyup', '#sell.center', function () {
+      moneyCalc();
+    });
   });
-});
 
-function moneyCalc() {
-  let inputNum = $('#sell_center').val();
-  let Input = parseInt(inputNum);
-  if (Input < 300 || Input > 9999999 || inputNum == "") {
-    $(".sales-commission").text("---");
-    $(".sales-profit").text("---");
-  } else {
-    let fee = parseInt(Input / 10);
-    $(".sales-commission").text("¥" + fee.toLocaleString());
-    $(".sales-profit").text("¥" + (Input - fee).toLocaleString());
+  function moneyCalc() {
+    let inputNum = $('#sell_center').val();
+    let Input = parseInt(inputNum);
+    if (Input < 300 || Input > 9999999 || inputNum == "") {
+      $(".sales-commission").text("-");
+      $(".sales-profit").text("-");
+    } else {
+      let fee = parseInt(Input / 10);
+      $(".sales-commission").text("¥" + fee.toLocaleString());
+      $(".sales-profit").text("¥" + (Input - fee).toLocaleString());
+    };
   };
-};
+});
