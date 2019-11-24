6654ea663d19c9d85d53e1ca732e3481e4f0aaca
toa0904 <pazutomo0428@icloud.com>
2019-11-24 17:01:16 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_23_051059) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -20,7 +19,6 @@ ActiveRecord::Schema.define(version: 2019_11_23_051059) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "cards", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "customer_id", null: false
@@ -28,7 +26,6 @@ ActiveRecord::Schema.define(version: 2019_11_23_051059) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,7 +43,6 @@ ActiveRecord::Schema.define(version: 2019_11_23_051059) do
     t.datetime "updated_at", null: false
     t.index ["user_id_id"], name: "index_items_on_user_id_id"
   end
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
