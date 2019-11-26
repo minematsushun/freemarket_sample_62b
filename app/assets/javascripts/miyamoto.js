f268839f1ed06e7b589c25cbd71c1aff1885d203
miyamo93 <miyamo0802@gmail.com>
2019-11-26 21:55:28 +0900

    Update miyamoto.js

--- app/assets/javascripts/miyamoto.js
+++ app/assets/javascripts/miyamoto.js
@@ -1,11 +1,14 @@
 $(document).on('turbolinks:load', function () {
   $('#upload-image').on('change', function (e) {
+    var preview = $(`#preview`)
     var reader = new FileReader();
     reader.onload = function (e) {
-      $("#preview").attr('src', e.target.result);
+      $(preview).attr('src', e.target.result);
+      var btn_wrapper = $('<div class="btn_wrapper"><div class="btn_edit">編集</div><div class="btn_delete">削除</div></div>');
+      $('#btn').append(btn_wrapper);
     }
     reader.readAsDataURL(e.target.files[0]);
   });
 });
-
-
+  $('btn_delete').on('click', '.delete', function() {
+})
