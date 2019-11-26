21018106565084eee3a237dfe2f128123985910a
miyamo93 <miyamo0802@gmail.com>
2019-11-26 16:35:41 +0900

    まいぐれーしょん

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_25_052941) do
+ActiveRecord::Schema.define(version: 2019_11_26_030601) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
