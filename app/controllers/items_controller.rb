a5de96f79c868dce6e8925979d37e0f0ba63816e
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-27 20:35:17 +0900

    出品完了後トップへ移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,7 +43,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save
+    if @item.save
+      redirect_to root_path, notice: '出品完了しました！'
+    else
+      render :new
+    end
   end
 
   private
