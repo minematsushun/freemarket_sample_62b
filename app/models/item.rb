583fab25da6c24353ef3ddae20befc443241656a
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-27 18:52:21 +0900

    変更6

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
 
-
+  belongs_to :seller, class_name: "user"
+  belongs_to :buyer, class_name: "user"
   belongs_to :category
   belongs_to :delivery
+  belongs_to :bland
 
 end
