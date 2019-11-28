ae2a83276225727c58794e8bb494c66b51d232a3
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-11-28 19:15:39 +0900

    登録してない時

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -9,17 +9,27 @@ class PurchaseController < ApplicationController
 
   def index
     @card = Card.find_by(user_id: current_user.id)
+      
+    if @card.blank?
 
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    else
+
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"] 
 
       customer = Payjp::Customer.retrieve(@card.customer_id)
 
       @default_card_information = customer.cards.retrieve(@card.card_id)
     end
+  end
 
   
   def pay
     card = Card.find_by(user_id: current_user.id)
+
+    if @card.blank?
+    redirect_to controller: "card", action: "new"
+
+    else
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     :amount => 10000, 
@@ -29,6 +39,8 @@ class PurchaseController < ApplicationController
   redirect_to action: 'done'
   
   end
+end
+
 
   def done
     @card = current_user.cards.first
