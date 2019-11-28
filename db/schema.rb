bdade5ed8f37bdd55178be94b78bcdc9e0f2b7ad
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-11-28 18:49:09 +0900

    自動生成ファイル類

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_053007) do
-
+ActiveRecord::Schema.define(version: 2019_11_28_090557) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -72,6 +71,15 @@ ActiveRecord::Schema.define(version: 2019_11_27_053007) do
     t.index ["user_id_id"], name: "index_items_on_user_id_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -80,8 +88,6 @@ ActiveRecord::Schema.define(version: 2019_11_27_053007) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
     t.string "nickname", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
@@ -109,4 +115,5 @@ ActiveRecord::Schema.define(version: 2019_11_27_053007) do
   add_foreign_key "items", "blands"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "deliveries"
+  add_foreign_key "sns_credentials", "users"
 end
