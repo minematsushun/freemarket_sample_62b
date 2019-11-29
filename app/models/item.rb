f317c2cc66c31999212864fdc8e1b6cec7ea9fb4
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-11-29 19:56:37 +0900

    追記２

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,12 @@
 class Item < ApplicationRecord
 
-  # belongs_to :seller, class_name: "user"
-  # belongs_to :buyer, class_name: "user"
-  belongs_to_active_hash        :prefecture
-  belongs_to :user
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id', optional: true
+  # belongs_to :user, optional: true
   belongs_to :category
   belongs_to :delivery
   belongs_to :bland
-  belongs_to :item, optional: true
+  # belongs_to :item, optional: true
   
   mount_uploader :image, ImageUploader
 
