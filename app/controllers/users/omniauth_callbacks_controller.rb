7f2868ff56439aa4bc61a4ad66347659e8f50a93
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-11-30 17:34:30 +0900

    CallBack　の書き換え

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,20 +7,37 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_from(:google)
   end
 
-  private
-
+  # common callback method
   def callback_from(provider)
     provider = provider.to_s
-
     info = User.find_for_oauth(request.env['omniauth.auth'])
-    @user = info[:user]
+    # snsの情報userが登録されているか or snsから情報を取得できているかを確認
+    @user = User.where(nickname: info[:user][:nickname]).or(User.where(email: info[:user][:email])).first || info[:user]
+
+    # persisted?はデータがDBに保存されているかを確認する/配列に対しては使えないから@userを定義するときは気をつける
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      #保存されていればログインしてroot_pathにリダイレクトされる
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
     else
-      @sns = info[:sns]
+      # 登録するアクションに取得した値を渡すために。sessionを利用してuserインスタンスを作成する
+      session[:nickname] = info[:user][:nickname]
+      session[:email] = info[:user][:email]
+
+      #snsでのユーザ登録ではパスワードを入力させないので準備する。パスワードを作成するDeviseメソッドもある。
+      #今回のバリデーションは英数字のみなのでこっちを採用
+      session[:password_confirmation] = SecureRandom.alphanumeric(30)
+
+      #SnsCredentialが登録されていないとき
+      if SnsCredential.find_by(uid: info[:sns][:uid], provider: info[:sns][:provider]).nil?
+        #ユーザ登録と同時にsns_credentialも登録するために
+        session[:uid] = info[:sns][:uid]
+        session[:provider] = info[:sns][:provider]
+      end
+      #登録フォームにリダイレクト
       redirect_to step1_signup_index_path
     end
+    # binding.pry
   end
 
   def failure
