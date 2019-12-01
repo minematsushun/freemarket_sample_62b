6c05e073960020ccd8ab1343fcdfae262115d2b5
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-12-01 19:32:47 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,6 @@ class ItemsController < ApplicationController
   def edit
     if user_signed_in? && current_user.id == @item.user_id_id
 
-
     @category_parent_array = Category.roots
     @category_child_array = @item.category.parent.parent.children
     @category_grandchild_array = @item.category.parent.children
@@ -108,7 +107,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path, notice: '出品完了しました！'
     else
-      redirect_to new_item_path, alert: '必須項目を埋めてください。'
+      render new, alert: '必須項目を埋めてください。'
     end
   end
 
