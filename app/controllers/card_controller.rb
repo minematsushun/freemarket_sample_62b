e8bf281ac0359a67e1493341e77cbe0d37394a9d
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2019-12-01 20:47:25 +0900

    1

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,12 +1,25 @@
 class CardController < ApplicationController
+  before_action :set_card , only: [:show] 
+  before_action :move_to_index 
 
   require "payjp"
 
   def new
     card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    # redirect_to action: "show" if card.exists?
   end
   
+  def show #Cardのデータpayjpに送り情報を取り出し
+    card = Card.find_by(user_id: current_user.id)
+    if card.blank?
+      redirect_to action: "new" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
   def pay #payjpとCardのデータベース作成
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     if params['payjp-token'].blank?
@@ -20,7 +33,7 @@ class CardController < ApplicationController
       ) 
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show"
+        redirect_to "/card/#{@card.id}"
       else
         redirect_to action: "pay"
       end
@@ -36,17 +49,21 @@ class CardController < ApplicationController
       customer.delete
       card.delete
     end
-      redirect_to  controller: :green , action: :pay
+      redirect_to   action: :index
   end
 
-  def show #Cardのデータpayjpに送り情報を取り出し
-    card = Card.find_by(user_id: current_user.id)
-    if card.blank?
-      redirect_to action: "new" 
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+
+  def index
+  end
+
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? 
+    redirect_to root_path
     end
   end
 end
