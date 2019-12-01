2a7028e6d3cf9a3dac1a2762e09888ed7b9aa8a4
miyamo93 <miyamo0802@gmail.com>
2019-12-01 16:02:10 +0900

    カード登録していない場合のボタン

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -6,18 +6,18 @@ class CardController < ApplicationController
     card = Card.where(user_id: current_user.id)
     redirect_to action: "show" if card.exists?
   end
-
+  
   def pay #payjpとCardのデータベース作成
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
-      description: '登録テスト',
-      email: current_user.email,
+      description: '登録テスト', 
+      email: current_user.email, 
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
-      )
+      ) 
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "show"
@@ -42,7 +42,7 @@ class CardController < ApplicationController
   def show #Cardのデータpayjpに送り情報を取り出し
     card = Card.find_by(user_id: current_user.id)
     if card.blank?
-      redirect_to action: "new"
+      redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
