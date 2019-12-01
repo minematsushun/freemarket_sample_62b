7062976def22ef5a625bbdc872f5d70237bbf655
toa0904 <pazutomo0428@icloud.com>
2019-12-01 18:20:40 +0900

    user_signed_in?の追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,7 +58,7 @@ class ItemsController < ApplicationController
 
 
   def destroy
-    if current_user.id == @item.seller_id
+    if user_signed_in? && current_user.id == @item.seller_id
       if @item.destroy
         redirect_to(root_path)
       else
