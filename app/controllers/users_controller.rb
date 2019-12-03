b1efad44f2a93b0567c35473b7dc8fa08c6f0bc8
minematsushun <50306222+minematsushun@users.noreply.github.com>
2019-12-03 15:50:10 +0900

    usersコントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   end
 
   def identification #本人確認
+    @user = User.find(params[:id])
   end
 
   def update
@@ -29,7 +30,21 @@ class UsersController < ApplicationController
   end
 
   def params_profile
-    params.require(:user).permit(:nickname, :introduce)
+    params.require(:user).permit(
+      :nickname,
+      :email,
+      :password,
+      :address_last_name,
+      :address_first_name,
+      :address_last_name_kana,
+      :address_first_name_kana,
+      :post_code,
+      :address_prefecture,
+      :address_city,
+      :address_number,
+      :address_building,
+      :address_phone_number,
+      :introduce)
   end
 
 end
